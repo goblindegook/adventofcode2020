6f1189d33d9e03f8599e2e3d2b0a3070063faa07
Luis Rodrigues <luis.rodrigues@springernature.com>
2020-12-22 10:13:19 +0000

    Advent of Code 2020: Day 12 (Part 2)

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -1,12 +1,14 @@
 package com.goblindegook.adventofcode2020
 
 import kotlin.math.PI
+import kotlin.math.absoluteValue
 import kotlin.math.cos
 import kotlin.math.sin
 
 fun main() {
     val instructions = object {}.javaClass.getResource("/day12-input.txt").readText()
     println(navigateShip(instructions))
+    println(navigateShipByWaypoint(instructions))
 }
 
 fun navigateShip(instructions: String): Int = instructions
@@ -14,11 +16,37 @@ fun navigateShip(instructions: String): Int = instructions
     .map { it.substring(0, 1) to (it.substring(1).toIntOrNull() ?: 0) }
     .fold(0 to 0) { (distance, degrees), (command, value) ->
         when (command) {
-            "S", "E"  -> distance + value to degrees
-            "N", "W"  -> distance - value to degrees
-            "F"  -> distance + value * (degrees * PI / 180.0).let { cos(it).toInt() - sin(it).toInt() } to degrees
-            "L"  -> distance to degrees + value
-            "R"  -> distance to degrees - value
+            "S", "E" -> distance + value to degrees
+            "N", "W" -> distance - value to degrees
+            "F" -> distance + value * factor(degrees) to degrees
+            "L" -> distance to degrees + value
+            "R" -> distance to degrees - value
             else -> distance to degrees
         }
-    }.first
+    }.first.absoluteValue
+
+fun navigateShipByWaypoint(instructions: String): Int = instructions
+    .lines()
+    .map { it.substring(0, 1) to (it.substring(1).toIntOrNull() ?: 0) }
+    .fold((0 to 0) to (10 to -1)) { (ship, waypoint), (command, value) ->
+        when (command) {
+            "S" -> ship to waypoint + (0 to value)
+            "E" -> ship to waypoint + (value to 0)
+            "N" -> ship to waypoint + (0 to -value)
+            "W" -> ship to waypoint + (-value to 0)
+            "F" -> ship + (waypoint * value) to waypoint
+            "L" -> ship to waypoint.rotate(-value)
+            "R" -> ship to waypoint.rotate(value)
+            else -> ship to waypoint
+        }
+    }.first.let { (x, y) -> x.absoluteValue + y.absoluteValue }
+
+private inline val Int.radianValue: Double get() = this * PI / 180.0
+
+private fun factor(deg: Int) = cos(deg.radianValue).toInt() - sin(deg.radianValue).toInt()
+
+private fun Pair<Int, Int>.rotate(deg: Int) =
+    (this * cos(deg.radianValue).toInt()) + ((-second to first) * sin(deg.radianValue).toInt())
+
+private inline operator fun Pair<Int, Int>.plus(pair: Pair<Int, Int>) = first + pair.first to second + pair.second
+private inline operator fun Pair<Int, Int>.times(m: Int) = first * m to second * m
--- src/test/kotlin/TestDay12.kt
+++ src/test/kotlin/TestDay12.kt
@@ -2,13 +2,15 @@ package com.goblindegook.adventofcode2020
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class TestDay12 {
 
     @Test
     fun `distance is -1 after moving one unit north`() {
         val input = "N1"
-        assertEquals(-1, navigateShip(input))
+        assertEquals(1, navigateShip(input))
     }
 
     @Test
@@ -26,7 +28,7 @@ class TestDay12 {
     @Test
     fun `distance is -1 after moving one unit west`() {
         val input = "W1"
-        assertEquals(-1, navigateShip(input))
+        assertEquals(1, navigateShip(input))
     }
 
     @Test
@@ -50,7 +52,7 @@ class TestDay12 {
             L90
             F1
         """.trimIndent()
-        assertEquals(-1, navigateShip(input))
+        assertEquals(1, navigateShip(input))
     }
 
     @Test
@@ -60,7 +62,7 @@ class TestDay12 {
             L90
             F1
         """.trimIndent()
-        assertEquals(-1, navigateShip(input))
+        assertEquals(1, navigateShip(input))
     }
 
     @Test
@@ -70,7 +72,7 @@ class TestDay12 {
             R90
             F1
         """.trimIndent()
-        assertEquals(-1, navigateShip(input))
+        assertEquals(1, navigateShip(input))
     }
 
     @Test
@@ -81,7 +83,7 @@ class TestDay12 {
             R90
             F1
         """.trimIndent()
-        assertEquals(-1, navigateShip(input))
+        assertEquals(1, navigateShip(input))
     }
 
     @Test
@@ -90,13 +92,13 @@ class TestDay12 {
             R180
             F1
         """.trimIndent()
-        assertEquals(-1, navigateShip(input))
+        assertEquals(1, navigateShip(input))
     }
 
     @Test
     fun `distance is -2 after moving two units north`() {
         val input = "N2"
-        assertEquals(-2, navigateShip(input))
+        assertEquals(2, navigateShip(input))
     }
 
     @Test
@@ -114,7 +116,7 @@ class TestDay12 {
     @Test
     fun `distance is -2 after moving two units west`() {
         val input = "W2"
-        assertEquals(-2, navigateShip(input))
+        assertEquals(2, navigateShip(input))
     }
 
     @Test
@@ -124,7 +126,7 @@ class TestDay12 {
     }
 
     @Test
-    fun `example `() {
+    fun `example instructions`() {
         val input = """
             F10
             N3
@@ -135,4 +137,115 @@ class TestDay12 {
         assertEquals(25, navigateShip(input))
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = ["N1", "S1", "E1", "W1", "L90", "R90"])
+    fun `ship does not move if only the waypoint moves`(input: String) {
+        assertEquals(0, navigateShipByWaypoint(input))
+    }
+
+    @Test
+    fun `forward moves the ship from (0, 0) to the waypoint at (10, -1)`() {
+        val input = "F1"
+        assertEquals(11, navigateShipByWaypoint(input))
+    }
+
+    @Test
+    fun `forward twice moves the ship from (0, 0) to (20, -2)`() {
+        val input = "F2"
+        assertEquals(22, navigateShipByWaypoint(input))
+    }
+
+    @Test
+    fun `shifting the waypoint south two units moves the ship from (0, 0) to (10, 1)`() {
+        val input = """
+            S2
+            F1
+        """.trimIndent()
+        assertEquals(11, navigateShipByWaypoint(input))
+    }
+
+    @Test
+    fun `shifting the waypoint north two units moves the ship from (0, 0) to (10, -3)`() {
+        val input = """
+            N2
+            F1
+        """.trimIndent()
+        assertEquals(13, navigateShipByWaypoint(input))
+    }
+
+    @Test
+    fun `shifting the waypoint west two units moves the ship from (0, 0) to (8, -1)`() {
+        val input = """
+            W2
+            F1
+        """.trimIndent()
+        assertEquals(9, navigateShipByWaypoint(input))
+    }
+
+    @Test
+    fun `shifting the waypoint east two units moves the ship from (0, 0) to (12, -1)`() {
+        val input = """
+            E2
+            F1
+        """.trimIndent()
+        assertEquals(13, navigateShipByWaypoint(input))
+    }
+
+    @Test
+    fun `rotating the waypoint left 90 degrees moves the ship from (0, 0) to (-1, -10)`() {
+        val input = """
+            L90
+            F1
+        """.trimIndent()
+        assertEquals(11, navigateShipByWaypoint(input))
+    }
+
+    @Test
+    fun `rotating the waypoint right 90 degrees moves the ship from (0, 0) to (1, 10)`() {
+        val input = """
+            R90
+            F1
+        """.trimIndent()
+        assertEquals(11, navigateShipByWaypoint(input))
+    }
+
+    @Test
+    fun `rotating the waypoint left 180 degrees is the same as rotating the waypoint right 180 degrees`() {
+        val left = """
+            L180
+            F1
+        """.trimIndent()
+        val right = """
+            R180
+            F1
+        """.trimIndent()
+
+        assertEquals(navigateShipByWaypoint(left), navigateShipByWaypoint(right))
+    }
+
+    @Test
+    fun `rotating the waypoint left 270 degrees is the same as rotating the waypoint right 90 degrees`() {
+        val left = """
+            L270
+            F1
+        """.trimIndent()
+        val right = """
+            R90
+            F1
+        """.trimIndent()
+
+        assertEquals(navigateShipByWaypoint(left), navigateShipByWaypoint(right))
+    }
+
+    @Test
+    fun `example instructions using waypoint`() {
+        val input = """
+            F10
+            N3
+            F7
+            R90
+            F11
+        """.trimIndent()
+        assertEquals(286, navigateShipByWaypoint(input))
+    }
 }
